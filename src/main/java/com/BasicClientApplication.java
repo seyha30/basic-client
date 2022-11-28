f0d0189388fe30fe57f88cd9356f79acfc21d9d9
seyha <sinseyha1x@gmail.com>
2022-11-28 16:53:14 +0700

    12

--- src/main/java/com/BasicClientApplication.java
+++ src/main/java/com/BasicClientApplication.java
@@ -1,13 +1,23 @@
 package com;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BasicClientApplication {
 
 	public static void main(String[] args) {
-	  
+		WebClient client = WebClient.builder()
+				  .baseUrl("http://localhost:8080")
+				  .defaultCookie("cookieKey", "cookieValue")
+				  .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE) 
+				  .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8080"))
+				  .build();
 		SpringApplication.run(BasicClientApplication.class, args);
 	}
 
